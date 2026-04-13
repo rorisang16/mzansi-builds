33cbbc562eaa789b389ba8d08d06f6197f129166
rorisang16 <rorisang.pitso01@gmail.com>
2026-04-13 14:18:40 +0200

    feat: add project schema

---
+++ backend/src/middleware/schemas/projectSchema.js
@@ -0,0 +1,27 @@
+import Joi  from "joi";
+
+export const createProjectSchema = Joi.object({ //joi.object defines what data must look like
+    title: Joi.string().min(3).max(100).required().messages({
+        //these are custom error messages that are more human readable versus using jois default ones
+        "string.min": "Title must be at least 3 characters",
+        "string.max": "Title must not exceed 100 characters",
+        "any.required": "Title is required",
+    }),
+
+    description: Joi.string().min(10).max(500).required()
+    .messages({
+      "string.min": "Description must be at least 10 characters",
+      "string.max": "Description must not exceed 500 characters",
+      "any.required": "Description is required",
+    }),
+  stage: Joi.string()
+    .valid('ideation', 'development', 'testing')
+    .required()
+    .messages({
+      "any.only": "Stage must be ideation, development or testing",
+      "any.required": "Stage is required",
+    }),
+     status: Joi.string()
+    .valid('active', 'completed')
+    .default('active'),
+});
